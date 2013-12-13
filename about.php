9822767c2834b658504261845e7b90032e1f6a65
David <david@mikroskil.ac.id>
2013-12-13 15:03:32 +0700

    merapikan coding

---
+++ about.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>WEBMATIKA</title>
+    <link href="assets/css/layout.css" rel="stylesheet" type="text/css">
+</head>
+<body bgcolor="#99FFCC">
+    <div id="container">
+        <header>
+            <ul id="nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="materi.php">Materi</a></li>
+                <li><a href="latihan.php">Latihan</a></li>
+                <li><a href="about.php">About</a></li>		
+            </ul>		
+        </header>
+        <article id="content" class="layer">
+            <h1>text...</h1>
+            <section>
+                <h2>text...</h2>        
+                <p>text...</p>
+            </section>
+        </article>
+        <footer>
+      	    <div>&copy; Copyright 2013</div>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ latihan.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>WEBMATIKA</title>
+    <link href="assets/css/layout.css" rel="stylesheet" type="text/css">
+</head>
+<body bgcolor="#99FFCC">
+    <div id="container">
+        <header>
+            <ul id="nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="materi.php">Materi</a></li>
+                <li><a href="latihan.php">Latihan</a></li>
+                <li><a href="about.php">About</a></li>		
+            </ul>		
+        </header>
+        <article id="content" class="layer">
+            <h1>Latihan</h1>
+            <section>
+                text...<br/>     
+                text...<br/>
+                text...<br/>
+            </section>
+        </article>
+        <footer>
+      	    <div>&copy; Copyright 2013</div>
+        </footer>
+    </div>
+</body>
+</html>
