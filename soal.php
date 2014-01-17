6e0d07ddbe2540cb604aac479897f1a6789e5a25
handy77 <handy_v@yahoo.com>
2014-01-17 10:31:17 +0700

    fix menu

--- soal.php
+++ soal.php
@@ -2,6 +2,8 @@
 require_once __DIR__.'/function/latihan.php';
 require_once __DIR__.'/function/library.php';
 
+$username = getUsername();
+
 if ($_GET['action'] == 'edit'){
     $soal = findOneSoal($_GET['id'], $_GET['tipe']);
     $title = 'Edit Soal';
@@ -16,21 +18,11 @@ if ($_GET['action'] == 'edit'){
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>WEBMATIKA</title>
-    <link href="assets/css/layout.css" rel="stylesheet" type="text/css">
-    <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <?php include_once "title.php";?>
 </head>
 <body>
     <div id="container">
-        <header>
-            <ul id="nav">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="materi.php">Materi</a></li>
-                <li><a href="latihan.php">Latihan</a></li>
-                <li><a href="about.php">About</a></li>
-            </ul>       
-        </header>
+        <?php include_once "header.php";?>
         <article id="content" class="layer">
             <h1><?php echo $title; ?></h1>
             <section>
